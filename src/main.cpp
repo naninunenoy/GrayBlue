ed26b1c81ea426d79c4d7d27f9687f17341b0b83
nakano yosuke <naninuneno0411@gmail.com>
2019-01-30 00:54:29 +0900

    hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-#include <Arduino.h>
+//#include <Arduino.h>
+#include <M5Stack.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// the setup routine runs once when M5Stack starts up
+void setup(){
+
+  // Initialize the M5Stack object
+  M5.begin();
+
+  // LCD display
+  M5.Lcd.printf("hello world");
 }
 
+// the loop routine runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
